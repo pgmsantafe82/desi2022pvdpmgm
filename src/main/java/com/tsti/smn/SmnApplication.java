a16aa0a8b9a3fa609d281a9d5a875caead3d9710
dardokuttel <dardokuttel@gmail.com>
2022-10-09 19:47:23 -0300

    merge resuelto

--- src/main/java/com/tsti/smn/SmnApplication.java
+++ src/main/java/com/tsti/smn/SmnApplication.java
@@ -8,6 +8,7 @@ public class SmnApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SmnApplication.class, args); //inicia la aplicación
+		//modificación remota
 		
 	}
 
